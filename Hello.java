393c33d1ac479a2eedeb4e31ff67e177a71fde1b
omipapal12 <omipapal12@gmail.com>
2021-07-10 09:33:37 +0530

    Program Added

--- Hello.java
+++ Hello.java
@@ -4,7 +4,10 @@ class Hello
 	{
 		System.out.println("Hello world");	
 int IS_FULL_TIME = 1;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+int wagePerHour = 20;
+int fullDayHour = 8;
+int dailyWage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
 			System.out.println("Employee is Present");
 		else
